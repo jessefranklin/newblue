b4e6d7ac3bbc33cb019b9ffcfe8e7683580268b5
Yonah <yonah@franklingood.ca>
2018-08-29 14:16:59 -0400

    Muliple Hosts Select
    Changes the host drop down into a multi select field
    
    Javascript added to avoid the need to hold CTRL while selecting multiple hosts

--- plugins/eventon-action-user/assets/js/au_script_f.js
+++ plugins/eventon-action-user/assets/js/au_script_f.js
@@ -722,6 +722,20 @@ jQuery(document).ready(function($){
 
 	    });
 	}
+	
+	$('option').mousedown(function(e) {
+		e.preventDefault();    
+		var originalScrollTop = $(this).parent().scrollTop();
+		console.log(originalScrollTop);
+		$(this).prop('selected', $(this).prop('selected') ? false : true);
+		var self = this;
+		$(this).parent().focus();
+		setTimeout(function() {
+			$(self).parent().scrollTop(originalScrollTop);
+		}, 0);
+		
+		return false;
+	});
 
 
 });
